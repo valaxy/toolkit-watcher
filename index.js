2729cf353ce2285b5c7f833a9a8c0048414e909f
valaxy <valaxy@qq.com>
2015-10-10 20:13:38 +0800

    add commander

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-var path = require('path')
+var commander = require('commander'),
+    fs        = require('fs'),
+    main      = require('./lib/main')
 
 
-var projectWatcher = require('./lib/project-watcher'),
-    toolboxWatcher = require('./lib/toolbox-watcher')
+var pkg = JSON.parse(fs.readFileSync(__dirname + '/package.json', {encoding: 'utf-8'}))
+commander
+	.version(pkg.version)
+	.option('--project <project>', 'project root')
+	.parse(process.argv)
 
 
-if (!module.parent) {
-	var ROOT = path.join(__dirname, './demo')
-	projectWatcher.init(ROOT)
-	toolboxWatcher.init(projectWatcher)
-	projectWatcher.start()
-	toolboxWatcher.start()
+if (!commander.project) {
+	console.error('argument project needed')
+	return
 }
 
+
+main(commander)
