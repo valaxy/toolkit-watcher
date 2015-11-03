9459cab03094fd36ebc22a461ca3aab90907cf7f
valaxy <valaxy@qq.com>
2015-11-03 19:02:10 +0800

    fix bug about FileGraph

--- test/file-graph.js
+++ test/file-graph.js
@@ -14,8 +14,12 @@ describe('FileGraph', function () {
 		fg.onChangeFile('b', ['a', 'c'])
 		assert.deepEqual(['b'], fg.getFilesUsed('a'))
 
+		fg.onAddFile('a', ['d'])
+		assert.deepEqual(['a', 'b'], fg.getFilesUsed('d'))
+
 		fg.onDeleteFile('b')
 		assert.deepEqual([], fg.getFilesUsed('c'))
+		assert.deepEqual(['a'], fg.getFilesUsed('d'))
 	})
 
 	it('onAddFile()', function () {
@@ -23,7 +27,7 @@ describe('FileGraph', function () {
 		fg.onAddFile('a', ['a/1', 'a/2'])
 		assert.deepEqual(['a'], fg.getFilesUsed('a/1'))
 		fg.onAddFile('b', ['a/2'])
-		assert.deepEqual(['a', 'b'], fg.getFilesUsed('a/2'))
+		assert.deepEqual(['b', 'a'], fg.getFilesUsed('a/2'))
 	})
 
 	it('onDeleteFile()', function () {
@@ -32,12 +36,12 @@ describe('FileGraph', function () {
 		fg.onAddFile('b', ['1', '3'])
 		fg.onAddFile('c', ['1', '4'])
 		fg.onDeleteFile('a')
-		assert.deepEqual(['b', 'c'], fg.getFilesUsed('1'))
+		assert.deepEqual(['c', 'b'], fg.getFilesUsed('1'))
 	})
 
 	it('onChangeFile()', function () {
 		var fg = FileGraph.create()
-		fg.onAddFile('a', ['1', '2'])
+		fg.onChangeFile('a', ['1', '2'])
 		fg.onChangeFile('a', ['3', '4'])
 		assert.deepEqual([], fg.getFilesUsed('1'))
 		assert.deepEqual(['a'], fg.getFilesUsed('3'))
